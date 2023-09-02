23257c3fe53745957e66f610fd08b2c62c72b6f0
Muhammad Rehan <75864401+MuhammadRehanRasool@users.noreply.github.com>
2023-09-03 01:23:26 +0500

    Update

--- client/src/CONSTANT.js
+++ client/src/CONSTANT.js
@@ -1,6 +1,6 @@
 const DOMAINS = {
-  server: "http://204.236.252.122:4000/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
-  client: "http://204.236.252.122:4000/", // CHANGE WITH YOUR FRONTEND LINK (/ is MUST IN END)
+  server: "http://13.127.6.177:4000/", // CHANGE WITH YOUR BACKEND LINK (/ is MUST IN END)
+  client: "http://13.127.6.177:4000/", // CHANGE WITH YOUR FRONTEND LINK (/ is MUST IN END)
 };
 
 export const CONSTANT = {
